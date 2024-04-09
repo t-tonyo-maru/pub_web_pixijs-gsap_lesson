8e8db8c7bf1b19ff987e2ee4f9782d7e005e4d7e
t-tonyo-maru <tonyomaru108@gmail.com>
2024-04-09 23:46:32 +0900

    add: pixijs first code

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,16 @@
 import './reset.css'
+import * as PIXI from 'pixi.js'
+
+const run = async () => {
+  const app = new PIXI.Application()
+  await app.init({ background: '#1099bb', resizeTo: window })
+  document.body.appendChild(app.canvas)
+
+  const texture = await PIXI.Assets.load('https://pixijs.com/assets/bunny.png')
+  const bunny = new PIXI.Sprite(texture)
+  bunny.anchor.set(0.5)
+  bunny.position.set(app.screen.width / 2, app.screen.height / 2)
+  app.stage.addChild(bunny)
+}
+
+run()
